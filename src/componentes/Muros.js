3310a164a0e8c809f28c643a4b84cfee622b881f
Luis Ramirez <theleyendofthewolf@hotmail.com>
2020-02-17 22:25:54 -0600

    muros conectados a fauna

--- src/componentes/Muros.js
+++ src/componentes/Muros.js
@@ -1,74 +1,77 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Fauna from './faunadb'
 import Muro from './muro'
-import api from '../api'
-
 
 class Muros extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            muros: []
-        }
+  constructor(props){
+    super(props);
+    this.state = {
+      muros: []
     }
-  componentDidMount(){
-        this.fetchData()
-    }
-
-  fetchData = async () => {
-     try{
-        const data = await api.walls.list();  
-        this.setState({muros:data})
-     } catch(error){
-        console.log(error)
-        if(error){
-            var data = require('../data/walls.json')
-            this.setState({muros:data})
-        }
-    }
-  } 
-
-  deleteWall = async (idtoDelete) => {
-            console.log(idtoDelete)
-        try{
-            await api.walls.remove(idtoDelete);  
-                alert('borrado')
-                this.fetchData();
-            } catch(error){
-            console.log(error)
-        }
-    } 
-
-  listarMuros = (muros) =>{
-    return (   muros.map((muro) => (
-            <React.Fragment key={muro.id}>
-           <Muro 
-            nombre={muro.nombre}
-            grito={muro.grito}
-            ubicacion={muro.ubicacion}
-            español1={muro.linea1.español}
-            dovah1={muro.linea1.dovah}
-            español2={muro.linea2.español}
-            dovah2={muro.linea2.dovah}
-            español3={muro.linea3.español}
-            dovah3={muro.linea3.dovah}
-            español4={muro.linea4.español}
-            dovah4={muro.linea4.dovah}
-            />
-            <div>
-                <button className='btn btn-danger mr-2' onClick={() =>this.deleteWall(muro.id)}>Borrar {muro.id}</button>
-                <Link to={`muros/${muro.id}/editar`}><button className='btn btn-primary'>Editar</button></Link>
-            </div>
-            </React.Fragment>
-            ))) 
   }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData = async () =>{
+    try {
+      const res = await Fauna.data.get();
+      this.setState({muros: res.data})
+      console.log(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  borrar = (ref) =>{
+    if (window.confirm('borrar?')) {
+      try {
+      const conf = Fauna.data.delete(ref)
+      console.log(conf)
+    } catch (error) {
+        console.log(error)
+    }
+    }else{
+        console.log('cancelos')
+    }
+  }
+
+  enlistador = () =>{
+      console.log(this.state.muros.length )
+        if (this.state.muros.length >=1 ) {
+            return(
+            this.state.muros.map((muro)=>(
+                <div key={muro.ts}>
+                <Muro nombre={muro.data.nombre}
+                        grito={muro.data.grito}
+                        ubicacion={muro.data.ubicacion}
+                        español1={muro.data.linea1.español}
+                        dovah1={muro.data.linea1.dovah}
+                        español2={muro.data.linea2.español}
+                        dovah2={muro.data.linea2.dovah}
+                        español3={muro.data.linea3.español}
+                        dovah3={muro.data.linea3.dovah}
+                        español4={muro.data.linea4.español}
+                        dovah4={muro.data.linea4.dovah}
+                />
+                <button className='deleter btn btn-danger' onClick={()=>this.borrar(muro.ref.id)}>Borrar ref {muro.ref.id}</button>
+                <Link to={`/muros/${muro.ref.id}/editar`}>Editar ref {muro.ref.id}</Link>
+            </div>
+            ))
+            )        
+        }else{
+            return(<h1>No hay muros disponibles</h1>)
+        }
+    }
 
   render(){
         return(<React.Fragment>
                 <h1>Listado de muros</h1>
                 <div className='col-md-9 mx-auto'>
-                {this.listarMuros(this.state.muros)}
+                {this.enlistador()}
                 </div>
             </React.Fragment>
             )
--- src/componentes/nuevomuro.js
+++ src/componentes/nuevomuro.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Muro from './muro'
 import AlfabetoDragon from './alfabetoDragon'
-import api from '../api' //json server
-// import Fauna from './faunadb'
+
+import Fauna from './faunadb'
 
 class NewWall extends React.Component{
     state={
@@ -30,110 +30,34 @@ class NewWall extends React.Component{
     }
 
     handleClick = async (e) =>{
-      e.preventDefault()
-   //json server
-         try {
-            await api.walls.create(this.state.form)
-            console.log(this.state.form)
-            alert('success')   
-         } catch (error) {
-            console.log(error)
-            alert(error)
-         }
-      //json server
-      
-      // //fauna
-      // try {
-      //   const res = await Fauna.data.save(this.state.form)
-      //    console.log(res)
-      //    if (res) {
-      //      alert('Guardado correctamente') 
-      //    }
-      // } catch (error) {
-      //    console.log(error)
-      // }
-      // //fauna
+      e.preventDefault()      
 
+      try {
+        const res = await Fauna.data.save(this.state.form)
+         console.log(res)
+         if (res) {
+           alert('Guardado correctamente') 
+         }
+      } catch (error) {
+         console.log(error)
+      }
    } 
 
     render(){
         return(
             <div className='row'>
-             {/* <div className='col-sm-12'>
-            <AlfabetoDragon />
-               </div>  
-            <form id='agregar_muro' className='col-sm-6 info' autoComplete='off'>
-            <div className='form-group'>
-                  <label>Nombre</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='nombre' value={this.state.form.nombre}/> 
-               </div>
-               <div className='form-group'>
-                  <label>Grito</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='grito' value={this.state.form.grito}/>
-               </div>
-               <div className='form-group'>
-                  <label>l1 Dovah</label>
-                  <input onChange={this.onChange} className='form-control' type='l1dovah' name='l1dovah' value={this.state.form.l1dovah}/>
-               </div>
-               <div className='form-group'>
-                  <label>L1 Español</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='l1español'value={this.state.form.l1español}/>
-               </div>
-
-               <div className='form-group'>
-                  <label>l2 Dovah</label>
-                  <input onChange={this.onChange} className='form-control' type='l1dovah' name='l2dovah' value={this.state.form.l2dovah}/>
-               </div>
-               <div className='form-group'>
-                  <label>L2 Español</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='l2español'value={this.state.form.l2español}/>
-               </div>
-               <div className='form-group'>
-                  <label>l3 Dovah</label>
-                  <input onChange={this.onChange} className='form-control' type='l1dovah' name='l3dovah' value={this.state.form.l3dovah}/>
-               </div>
-               <div className='form-group'>
-                  <label>L3 Español</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='l3español'value={this.state.form.l3español}/>
-               </div>
-               <div className='form-group'>
-                  <label>l4 Dovah</label>
-                  <input onChange={this.onChange} className='form-control' type='l1dovah' name='l4dovah' value={this.state.form.l4dovah}/>
-               </div>
-               <div className='form-group'>
-                  <label>L4 Español</label>
-                  <input onChange={this.onChange} className='form-control' type='text' name='l4español'value={this.state.form.l4español}/>
-               </div>
-               <button  onClick={this.handleClick} className='btn btn-primary'>Guardar</button>
-        </form>
-        <div className='col-sm-6'>
-            <Muro 
-               nombre={this.state.form.nombre || "Nombre del muro"}
-               grito={this.state.form.grito || "Grito"}
-               español1={this.state.form.l1español}
-               dovah1={this.state.form.l1dovah}
-               español2={this.state.form.l2español}
-               dovah2={this.state.form.l2dovah}
-               español3={this.state.form.l3español}
-               dovah3={this.state.form.l3dovah}
-               español4={this.state.form.l4español}
-               dovah4={this.state.form.l4dovah}
-          />
-        </div> */}
-
          {/*carusel  */}
-
-         <div id="carouselExampleIndicators" class="carousel mx-auto col-md-9" data-interval="false" data-ride="carousel">
-            <ol class="carousel-indicators">
+         <div id="carouselExampleIndicators" className="carousel mx-auto col-md-9" data-interval="false" data-ride="carousel">
+            <ol className="carousel-indicators">
                <li data-target="#carouselExampleIndicators" data-slide-to="0"></li>
-               <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+               <li data-target="#carouselExampleIndicators" data-slide-to="1" className="active"></li>
                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
             </ol>
-               <div class="carousel-inner">
-                  <div class="carousel-item">
+               <div className="carousel-inner">
+                  <div className="carousel-item">
                      <AlfabetoDragon />
                   </div>
-                  <div class="carousel-item active">
+                  <div className="carousel-item active">
                     <form id='agregar_muro' className='info px-4' autoComplete='off'>
                      <div className='form-group'>
                            <label>Nombre</label>
