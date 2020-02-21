971dd0324fcc771d27a3019bdb840c81c5f86209
Luis Ramirez <theleyendofthewolf@hotmail.com>
2020-02-21 00:36:14 -0600

    renderizado optimizado a una pagina

--- src/componentes/selector.js
+++ src/componentes/selector.js
@@ -1,20 +1,81 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Model from './modelador'
+
 class Selector extends React.Component{
+    state = {isMounted: true, 
+             model:'spriggan'
+            }
+
+
+    getModel = (model) =>{
+        this.setState(state => 
+            ({isMounted: false, model:model}), 
+            ()=>this.renderAgain()
+             )
+
+    }
+     renderAgain = () =>{
+         this.setState({isMounted:true})
+     }       
     render(){
+        const {isMounted = true} = this.state;
+        const {model = 'spriggan'} = this.state;
+
         return(
             <div>
-                <nav className='navbar'>
-                    <Link className='btn btn-secondary' to={'./modelos/bear'}>Oso de cueva</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/esfera'}>esfera enana</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/spriggan'}>spriggan</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/aranaenana'}>Araña enana</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/cauro'}>cauro</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/centurion'}>Centurion</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/valleysaber'}>Gato sable del Valle</Link>
-                    <Link className='btn btn-secondary' to={'./modelos/vaca'}>Vaca</Link>
-                </nav>
+            <div className='row'>
+                <div className='col-md-4 flex-column'>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('valleysaber')}>
+                        valleysaber
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('esfera')}>
+                        esfera
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('bear')}>
+                        Oso de Cueva
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('centurion')}>
+                        Centurion Enano
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('cauro')}>
+                        Cauro
+                    </button>
+
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('aranaenana')}>
+                        Araña enana
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('spriggan')}>
+                        spriggan
+                    </button>
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('vaca')}>
+                        Vaca
+                    </button>
+
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('atronachhielo')}>
+                        Atronach de hielo
+                    </button>
+
+
+                    <button className='btn btn-secondary' onClick={()=> this.getModel('atronachtormenta')}>
+                        Atronach de la tormenta
+                    </button>
+
+
+                </div>
+                <div className='col-md-8'>
+                    {isMounted && 
+                    <div> 
+                        <Model 
+                            mtlfile={require(`./models/${model}.mtl`)} 
+                            objfile={require(`./models/${model}.obj`)} 
+                            pngfile={require(`./models/${model}.png`)}
+                        />
+                        </div>
+                    }
+                </div>
+            </div>
             </div>
         )
     }
